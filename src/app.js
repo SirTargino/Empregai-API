3245cb5472495f26af3c2db0d2980be78f640055
João Targino <joaoetargino@gmail.com>
2024-06-14 16:45:41 -0300

    feat: add new features to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { connectToDatabase } from "./database/connection.js";
+import { db } from "./database/connection.js";
+import { jobRouter } from "./routes/job.routes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(jobRouter);
+
 app.listen(4000, async () => {
-    connectToDatabase();
-    console.log(`Server run on port: 4000`);
+    try {
+        db.connect();
+        console.log(`Server run on port: 4000`);
+    } catch (error) {
+        console.error(error);
+    }
 });
